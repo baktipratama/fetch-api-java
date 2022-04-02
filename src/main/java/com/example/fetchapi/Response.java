46e4ac1b8df66b1d1e6419082454929ed79dfcfd
Bakti <baktiayp@gmail.com>
2022-04-02 18:36:47 +0700

    init spring-boot rest api

---
+++ src/main/java/com/example/fetchapi/Response.java
@@ -0,0 +1,14 @@
+package com.example.fetchapi;
+
+public class Response {
+
+    long data;
+
+    public Response(long data) {
+        this.data = data;
+    }
+
+    public long getData() {
+        return data;
+    }
+}
