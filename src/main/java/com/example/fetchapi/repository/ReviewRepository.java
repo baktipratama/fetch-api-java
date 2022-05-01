20bcb6956671126f55518ab164435e999892b34c
ayu <dyahayuadayu@gmail.com>
2022-05-01 15:12:21 +0700

    tunning db query

--- src/main/java/com/example/fetchapi/repository/ReviewRepository.java
+++ src/main/java/com/example/fetchapi/repository/ReviewRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, String> {
 
-    @Query(nativeQuery = true, value = "select reviewid, title, url, score, pub_year from reviews where score >= :score")
+    @Query(nativeQuery = true, value = "SELECT r.reviewid, r.title, r.url, r.score, (SELECT GROUP_CONCAT(a.artist)) as artists, (SELECT GROUP_CONCAT(g.genre)) as genres, (SELECT GROUP_CONCAT(l.label)) as labels, r.pub_year FROM reviews as r join labels as l on l.reviewid = r.reviewid join artists as a on a.reviewid = r.reviewid join content as c on c.reviewid = r.reviewid join genres as g on g.reviewid = r.reviewid WHERE r.score >= :score GROUP BY r.reviewid LIMIT 10")
     List<Review> getReviewsByScore(@Param("score") Integer score);
 }
